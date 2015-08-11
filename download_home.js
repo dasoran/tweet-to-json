25ed1dee1017815d9bef0b49f225e7fd2912d2cd
dasoran <kyota.yasuda@bizreach.co.jp>
2015-08-11 14:56:07 +0900

    ユーザーの重複を無くした。

--- download_home.js
+++ download_home.js
@@ -14,27 +14,32 @@ var params = {count: 200};
 client.get('statuses/home_timeline', params, function(error, tweets, response){
   if (!error) {
     //fs.writeFile(tweetjson, JSON.stringify(tweets));
+    var addedUserIds = {};
     for (var i = 0; i < tweets.length; i++) {
       var tweet = tweets[i];
 
-      // user first-line
-      var toJson = {};
-      toJson.index = {}
-      toJson.index._index = indexName;
-      toJson.index._type = 'user';
-      fs.appendFile(userjson, JSON.stringify(toJson) + '\n');
+      if (addedUserIds[tweet.user.id] != true) {
+        // user first-line
+        var toJson = {};
+        toJson.index = {}
+        toJson.index._index = indexName;
+        toJson.index._type = 'user';
+        fs.appendFile(userjson, JSON.stringify(toJson) + '\n');
 
-      // user second-line
-      var toJson = {};
-      toJson.id = tweet.user.id;
-      toJson.screen_name = tweet.user.screen_name;
-      toJson.name = tweet.user.name;
-      toJson.description = tweet.user.description;
-      var path = url.parse(tweet.user.profile_image_url).pathname;
-      toJson.profile_image_url = 'img' + path;
-      toJson.follow = [];
-      toJson.follower = [];
-      fs.appendFile(userjson, JSON.stringify(toJson) + '\n');
+        // user second-line
+        var toJson = {};
+        toJson.id = tweet.user.id;
+        toJson.screen_name = tweet.user.screen_name;
+        toJson.name = tweet.user.name;
+        toJson.description = tweet.user.description;
+        var path = url.parse(tweet.user.profile_image_url).pathname;
+        toJson.profile_image_url = 'img' + path;
+        toJson.follow = [];
+        toJson.follower = [];
+        fs.appendFile(userjson, JSON.stringify(toJson) + '\n');
+
+        addedUserIds[tweet.user.id] = true;
+      }
 
       // tweet first-line
       var toJson = {};
